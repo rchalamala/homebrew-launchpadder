166c81d027637906ec5ae887a649da87888b280e
Rahul Chalamala <rchalamala@outlook.com>
2025-07-03 23:16:35 -0700

    Fix formatting of LaunchPadder cask URL in launchpadder.rb

--- Casks/launchpadder.rb
+++ Casks/launchpadder.rb
@@ -2,7 +2,7 @@ cask "launchpadder" do
   version "1.4"
   sha256 "e6f22aec28546b3ab418cb06c9a26c696126c4ec4daeeaebd1a427a8c8b86390"
 
-  url "https://github.com/Molay/LaunchPadder/releases/download/v#{version}/LaunchPadder-v#{version}.zip",
+  url "https://github.com/Molay/LaunchPadder/releases/download/v#{version}/LaunchPadder-v#{version}.zip"
   name "LaunchPadder"
   desc "LaunchPad tool that organizes and sorts apps and folders"
   homepage "https://github.com/Molay/LaunchPadder"
